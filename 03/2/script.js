5722ec8bdd51dfb3e86e96b815a92c1ffdcf41b7
amolazinali <119149587+amolazinali@users.noreply.github.com>
2022-12-23 22:13:32 +0330

    Update script.js

---
+++ 03/2/script.js
@@ -0,0 +1,6 @@
+document.getElementById("btn").onclick = function() {
+    document.getElementById("fn").innerHTML =
+    document.getElementById("name-id").value;
+    document.getElementById("pw").innerHTML =
+    document.getElementById("pass-id").value;
+}
